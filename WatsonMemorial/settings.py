58f997744aa6a794ccdf2dda7b70e10a4d0b18df
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-17 23:47:51 -0500

    Update settings.py

--- WatsonMemorial/settings.py
+++ WatsonMemorial/settings.py
@@ -143,13 +143,7 @@ MEDIA_ROOT =  '/var/media'
 
 MEDIA_URL = '/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default=None)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=None)
 
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 
 TASKS = {
     "default": {
