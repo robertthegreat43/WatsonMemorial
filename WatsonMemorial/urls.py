c497153a6537fdc4bd735a5474f8c9e4936053c6
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-18 00:02:28 -0500

    Update urls.py

--- WatsonMemorial/urls.py
+++ WatsonMemorial/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
-
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
