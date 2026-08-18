da9b284244398c580313b1447567ff8b1eafa3b5
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-17 23:52:09 -0500

    Clean up URL configuration in urls.py
    Removed comments and cleaned up the URL patterns.

--- WatsonMemorial/urls.py
+++ WatsonMemorial/urls.py
@@ -19,11 +19,9 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
-from Events import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Events.urls')),
-    path('media_example/', views.media_example, name='media_example'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
