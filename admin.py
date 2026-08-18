90581a7f07a44a052d7d5c059659b110591394af
GameScience <122305309+robertthegreat43@users.noreply.github.com>
2026-08-17 21:46:06 -0500

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+
+
+
+class WatsonMemorialAdminSite(admin.AdminSite):
+    title = "Watson Memorial"
+    site_header = "Watson Memorial"
+    index_title = "Watson Memorial"
+
